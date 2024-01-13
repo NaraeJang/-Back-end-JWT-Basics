0ba030841690a20c4284f97605dd2d681659d28d
NaraeJang <j.naaaarae@gmail.com>
2024-01-13 11:49:35 -0500

    Auth Middleare Complete

--- controllers/main.js
+++ controllers/main.js
@@ -24,27 +24,12 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-  const authHeader = req.headers.authorization;
+  const luckyNumber = Math.floor(Math.random() * 100);
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    throw new CustomAPIError("No token provided", 401);
-  }
-
-  const token = authHeader.split(" ")[1];
-
-  // Check if token is expired.
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET); //we have to pass token & secret string.
-
-    const luckyNumber = Math.floor(Math.random() * 100);
-
-    res.status(200).json({
-      msg: `Hello, ${decoded.username}`,
-      secret: `Here is your authorized data, your luck number is ${luckyNumber}`,
-    });
-  } catch (error) {
-    throw new CustomAPIError("Not authorized to access this route", 401);
-  }
+  res.status(200).json({
+    msg: `Hello, ${req.user.username}`,
+    secret: `Here is your authorized data, your luck number is ${luckyNumber}`,
+  });
 };
 
 module.exports = { login, dashboard };
