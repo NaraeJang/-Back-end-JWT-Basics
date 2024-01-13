55608b9bd3a69c2e3398205417ccf6adb15646d4
NaraeJang <j.naaaarae@gmail.com>
2024-01-13 10:44:46 -0500

    Validate Username and Password

--- controllers/main.js
+++ controllers/main.js
@@ -1,9 +1,22 @@
+const CustomAPIError = require("../errors/custom-error");
+
 const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  //HOW TO CHECK IF THERE IS PROPER VALUES.
+  // mongoose validation
+  // Joi Library
+  // Check in the controller
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide email and password", 400);
+  }
+
   res.send("Fake Login/Register/Signup Route");
 };
 
 const dashboard = async (req, res) => {
-  const luckyNumber = Match.floor(Math.random() * 100);
+  const luckyNumber = Math.floor(Math.random() * 100);
+
   res.status(200).json({
     msg: `Hello, Narae Doe`,
     secret: `Here is your authorized data, your luck number is ${luckyNumber}`,
