284058e4cfa1a225e95723be49171e772b9177d9
NaraeJang <j.naaaarae@gmail.com>
2024-01-13 12:23:11 -0500

    Debugging

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,12 +1,12 @@
 const { CustomAPIError } = require("../errors");
-const { StatusCode } = require("http-status-codes");
+const { StatusCodes } = require("http-status-codes");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message });
   }
   return res
-    .status(StatusCode.INTERNAL_SERVER_ERROR)
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
     .send("Something went wrong try again later");
 };
 
